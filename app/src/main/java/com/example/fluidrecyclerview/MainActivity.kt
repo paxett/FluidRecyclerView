0b5f797b52cb83eec157df914ccf194b5329b35a
paxett <y.bogdanov@gmail.com>
2021-01-29 12:24:53 +0300

    Initial commit

---
+++ app/src/main/java/com/example/fluidrecyclerview/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.fluidrecyclerview
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val recyclerView : RecyclerView = findViewById(R.id.recycler)
+        recyclerView.layoutManager = GridLayoutManager(this, 2)
+    }
+}
